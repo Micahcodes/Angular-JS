a45b03c3c0de8a0350e36af730cdff5c35973406
Micah Stewart <stewart.micah@gmail.com>
2014-04-27 15:23:07 -0500

    Generate new rails app to play around with Angular

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularTutorial::Application.config.secret_key_base = 'd9662e6bb939e7869553f648601ab4e8f65d53840e401a7c3d7984092a015f18f35989034e06104b18d697d220c8e895fd092b5f8ac0ac9bd1843cbd2d4e2e6a'
